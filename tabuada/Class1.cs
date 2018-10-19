01f89dc59b99cc3613eea39267838207cd669605
Aluno <jdorian21@gmail.com>
2018-10-19 20:46:33 -0300

    1subir

---
+++ tabuada/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuada
+{
+    class Class1
+    {
+        public void Tabuada1()
+        {
+            int num, num2;
+            num = 1;
+
+            while (num <= 10)
+            {
+                num2 = 1;
+                while (num2 <= 10)
+                {
+                    Console.WriteLine("{0} x {1} = {2}", num, num2, num * num2);
+                    num2++;
+                }
+                Console.WriteLine("");
+                num++;
+            }
+        }
+        public void Exercicio2()
+        {
+
+        }
+        public void Exercicio3()
+        {
+            int num, soma;
+            for (num = 0; num <= 1000; num += 100)
+            {
+                Console.WriteLine(num);
+
+            }
+        }
+    }
+}
---
+++ tabuada/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+         // obj.Tabuada1();
+            obj.Exercicio3();
+            
+            Console.ReadKey();
+        }
+    }
+}
